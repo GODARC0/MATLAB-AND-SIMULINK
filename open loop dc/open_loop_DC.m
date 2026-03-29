2cc4957446f589992fbc1377c62fbfcf8ef679c0
GODARC0 <mohdluqmaanishere@gmail.com>
2026-03-29 10:24:38 +0530

    DC motor speed control (open loop)
    this is a simple dc motor speed control using open loop system

---
+++ open loop dc/open_loop_DC.m
@@ -0,0 +1,6 @@
+J = 0.01;  %Rotor moment of inertia
+b = 0.1;   %Viscous friction coefficient
+K = 0.01;  %Motor torque & back-EMF constant
+R = 1;     %Armature resistance
+L = 0.5;   %Armature inductance
+V_in = 12; %Step input voltage
